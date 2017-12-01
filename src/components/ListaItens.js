df9e9696cfbaaed4cf3ce7f3f2ae3ce4963f77c3
Rafael Pantoja <rafaelbp92@gmail.com>
2017-12-01 11:38:40 -0300

    Add componentes lista itens e itens e axios

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -4,11 +4,49 @@ import {
   Text
 } from 'react-native';
 
+import axios from 'axios';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
-  render() {
-    return (
-      <Text>teste de importação</Text>
-    );
-  }
+
+	/*
+
+	constructor(props) {
+		super(props);
+		console.log('Construindo a aplicação');
+	}
+
+	componentWillMount() {
+		console.log('Fazer alguma coisa antes de renderizar');
+	}
+
+	componentDidMount() {
+		console.log('Fazer alguma coisa depois de renderizar');
+	}
+
+  	render() {
+  		console.log('Componente é renderizado');
+		return (
+		<Text>teste de importação</Text>
+		);
+  	}
+  	*/
+  	constructor(props) {
+		super(props);
+		this.state = { listaItens : [] };
+	}
+
+ 	componentWillMount() {
+		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+		.then(response => { this.setState({ listaItens : response.data }); })
+		.catch(() => { console.log('Erro ao recuperar dados'); });
+	}
+
+  	render() {
+  		return (
+  			<View>
+  			{ this.state.listaItens.map( item =>   (<Text key={item.titulo}>{ item.titulo }</Text>))}
+  			</View>
+  		);
+ 	}
 }
