57fd036f2ab3fcb604761346614925faff1dd94b
= <=>
2017-12-04 13:39:03 -0300

    Melhoria no visual do App

--- src/components/Itens.js
+++ src/components/Itens.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Image
+  Image,
+  StyleSheet
 } from 'react-native';
 
 
@@ -11,13 +12,48 @@ export default class ListaItens extends Component {
 
 	render() {
 		return (
-			<View>
-			<Image style={{ height: 100, width: 100 }} source={{ uri: this.props.item.foto }} />
-				<Text>{this.props.item.titulo}</Text>
-				<Text>{this.props.item.valor}</Text>
-				<Text>{this.props.item.local_anuncio}</Text>
-				<Text>{this.props.item.data_publicacao}</Text>	  			
+			<View style={styles.item}>
+				<View style={styles.foto}>
+					<Image style={{ height: 100, width: 100 }} source={{ uri: this.props.item.foto }} />
+				</View>
+				<View style={styles.detalhesItens}>
+					<Text style={styles.txtTitulo}>{this.props.item.titulo}</Text>
+					<Text style={styles.txtValor}>{this.props.item.valor}</Text>
+					<Text style={styles.txtDetalhes}>{this.props.item.local_anuncio}</Text>
+					<Text>{this.props.item.data_publicacao}</Text>	
+				</View>  			
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	item: {
+		borderWidth: 0.5,
+		borderColor: '#999',
+		margin: 10,
+		padding: 10,
+		flexDirection: 'row',
+		backgroundColor: '#FFF' 
+	},
+	foto: {
+		width: 102,
+		height: 102
+	},
+	detalhesItens: {
+		marginLeft: 20,
+		flex: 1
+	},
+	txtTitulo: {
+		fontSize: 18,
+		color: 'blue',
+		marginBottom: 5
+	},
+	txtValor: {
+		fontSize: 16,
+		fontWeight: 'bold'
+	},
+	txtDetalhes: {
+		fontSize: 16
+	}
+});
